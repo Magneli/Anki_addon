c5842d92d2e1f31774a8921edceb523a0ca14348
Magne <magne.limi@gmail.com>
2015-09-21 20:38:49 +0200

    a

--- anki_api_interface.py
+++ anki_api_interface.py
@@ -7,19 +7,54 @@ from aqt.utils import showInfo
 from aqt.qt import *
 from anki import Collection
 
-deck_name = u'語彙'
+deck_name = "Test"
+
 
 def get_deck_name(card):
     return card.col.decks.name(card.did)
 
 
+def get_card(id):
+    return mw.col.getCard(id)
+
+
+def find_cards(search_term):
+    return mw.col.findCards(search_term)
+
+
 def is_card_in_deck(expression, reading):
-    ids = mw.col.findCards(expression)
+    ids = find_cards(expression)
     for id in ids:
-        card = mw.col.getCard(id)
+        card = get_card(id)
         if get_deck_name(card) == deck_name:
             note = card.note()
             if expression == note.items()[0][1] and reading == note.items()[1][1]:
                 return True
-
     return False
+
+
+def add_word_card(expression, reading, meaning):
+    card = _find_sample_card_for_card_creation()
+    note = card.col.newNote()
+    list = (expression, reading, meaning, "", "")
+    i = 0
+    for (name, value) in note.items():
+        note[name] = value + list[i]
+        i += 1
+    card.col.addNote(note)
+    note.flush()
+
+def _find_sample_card_for_card_creation():
+    ids = find_cards("deck:"+deck_name)
+    for id in ids:
+        if get_deck_name(get_card(id)) == deck_name:
+            return get_card(id)
+
+def reset_card_due_stats(card):
+    card.queue = 1
+    card.type = 2
+    card.due = card.col.sched.today
+    card.ivl = 1
+    card.factor = 2500
+    card.col.sched._updateStats(card, 'lrn')
+    card.flushSched()

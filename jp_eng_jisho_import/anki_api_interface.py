23c7e33cf187b5f3543983dccd746957344690a6
Magne <magne.limi@gmail.com>
2015-10-02 20:24:50 +0200

    Reworked inner working of card adding, now comes with a default deck\template setup

--- jp_eng_jisho_import/anki_api_interface.py
+++ jp_eng_jisho_import/anki_api_interface.py
@@ -8,7 +8,13 @@ from aqt.utils import showInfo
 from aqt.qt import *
 from anki import Collection
 
-deck_name = u'語彙'
+deck_name = u'Words'
+model_name = u'Default plugin template'
+
+
+# deck_name = u'語彙'
+# model_name = u'Japonaise example_sentence'
+
 
 
 def get_deck_name(card):
@@ -34,28 +40,6 @@ def is_card_in_collection(expression, reading):
     return False
 
 
-def add_word_card(expression, reading, meaning):
-    card = _find_sample_card_for_card_creation()
-    note = card.col.newNote()
-    new_card_data = (expression, reading, meaning)
-    i = 0
-    for (name, value) in note.items():
-        if i < 3:
-            note[name] = value + new_card_data[i]
-        else:
-            note[name] = value + ""
-        i += 1
-    card.col.addNote(note)
-    note.flush()
-
-
-def _find_sample_card_for_card_creation():
-    ids = find_cards("deck:current")
-    for id in ids:
-        # if get_deck_name(get_card(id)) == deck_name:
-        return get_card(id)
-
-
 def reset_card_due_stats(card):
     card.queue = 2
     card.type = 2
@@ -64,3 +48,71 @@ def reset_card_due_stats(card):
     card.factor = 1000
     card.col.sched._updateStats(card, 'rev')
     card.flushSched()
+
+
+def add_word_card(expression, reading, meaning):
+    deck = find_deck()
+    model = find_model(deck)
+    set_deck_id(model, deck['id'])
+    select_model(model)
+    note = make_note()
+    add_info_to_note(note, expression, reading, meaning)
+    mw.col.addNote(note)
+    note.flush()
+
+
+def add_info_to_note(note, expression, reading, meaning):
+    new_card_data = (expression, reading, meaning)
+    i = 0
+    for (name, value) in note.items():
+        if i < 3:
+            note[name] = value + new_card_data[i]
+        else:
+            note[name] = value + ""
+        i += 1
+
+
+def make_note():
+    return mw.col.newNote(False)
+
+
+def select_model(model):
+    mw.col.models.setCurrent(model)
+
+
+def find_deck():
+    deck = mw.col.decks.byName(deck_name)
+    if deck == None:
+        mw.col.decks.id(deck_name)
+    deck = mw.col.decks.byName(deck_name)
+    return deck
+
+
+def find_model(deck):
+    model = mw.col.models.byName(model_name)
+    if model == None:
+        add_model(model_name, deck['id'])
+    model = mw.col.models.byName(model_name)
+    return model
+
+
+def set_deck_id(model, did):
+    model['tmpls'][0]['did'] = did
+
+
+def add_model(name, id):
+    m = mw.col.models.new(name)
+    newfield = mw.col.models.newField("Expression")
+    newfield2 = mw.col.models.newField("Reading")
+    newfield3 = mw.col.models.newField("Meaning")
+    mw.col.models.addField(m, newfield)
+    mw.col.models.addField(m, newfield2)
+    mw.col.models.addField(m, newfield3)
+    template = mw.col.models.newTemplate("template")
+    template[
+        'qfmt'] = "<span style=\"font-family: Kozuka Gothic Pr6N M;   \"><span style=\"font-family: Kozuka Gothic Pr6N M; font-size: 40px; \">{{Expression}}</span></span>"
+    template[
+        'afmt'] = "{{FrontSide}} <hr id=answer> <span style=\"font-family: Kozuka Gothic Pr6N M;   \">{{Reading}}<br>{{Meaning}}<br><p align=\"left\">"
+    template['did'] = id
+    mw.col.models.addTemplate(m, template)
+    mw.col.models.add(m)

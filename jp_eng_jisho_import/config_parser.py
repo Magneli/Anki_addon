6a53f6e381ec66e334c65b631928525776711d17
Magne <magne.limi@gmail.com>
2015-10-05 07:47:54 +0200

    Working on deck selection

--- jp_eng_jisho_import/config_parser.py
+++ jp_eng_jisho_import/config_parser.py
@@ -1,23 +1,44 @@
 # -*- coding: utf-8 -*-
 # python 2.7
 
-import ConfigParser
 
-config = ConfigParser.ConfigParser()
+import codecs
+
+deck_name = None
+model_name = None
+
+
+def read_config():
+    global deck_name
+    try:
+        cfgfile = open("deck_name.ini", 'r')
+        deck_name = cfgfile.read()
+        cfgfile.close()
+    except:
+        print "wat"
+        pass
 
 
 def write_config():
-    cfgfile = open("config.ini", 'w')
-    config.add_section('Deck')
-    config.set('Deck', 'Deck name', '語彙')
-    config.set('Deck', 'Model', 'Japanese-eng words')
-    config.write(cfgfile)
+    cfgfile = open("deck_name.ini", 'w')
+
+    cfgfile.write("漢字")
     cfgfile.close()
 
-#write_config()
 
-config.read("config.ini")
-options = config.options('Deck')
-asd = config.get('Deck', 'Deck name')
-print config.sections()
-print asd
+def write_default_config():
+    cfgfile = open("deck_name.ini", 'w')
+    cfgfile.close()
+
+
+# write_default_config()
+# write_config()
+read_config()
+
+# deck_name = "かんじ"
+# model_name = "asdasd"
+print deck_name
+print model_name
+
+
+# write_config()
